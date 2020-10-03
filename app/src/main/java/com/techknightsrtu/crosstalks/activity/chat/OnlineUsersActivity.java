19f8ad74be01b46c923eb9f40fdecb0ef8290f78
Tushar Dave <imtushardave@gmail.com>
2020-10-03 15:12:51 +0530

    Basic App Skeleton completed
    App skeleton is ready, navigation between chats and OnlineUsersActivity is done.

--- app/src/main/java/com/techknightsrtu/crosstalks/activity/chat/OnlineUsersActivity.java
+++ app/src/main/java/com/techknightsrtu/crosstalks/activity/chat/OnlineUsersActivity.java
@@ -3,7 +3,6 @@ package com.techknightsrtu.crosstalks.activity.chat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -11,18 +10,15 @@ import android.widget.ImageView;
 import com.techknightsrtu.crosstalks.R;
 import com.techknightsrtu.crosstalks.activity.profile.ProfileActivity;
 
-public class HomeActivity extends AppCompatActivity {
-
-    private static final String TAG = "HomeActivity";
+public class OnlineUsersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_online_users);
 
         setupBottomNavigationBar();
     }
-
 
     private void setupBottomNavigationBar(){
 
@@ -30,26 +26,26 @@ public class HomeActivity extends AppCompatActivity {
         ImageView onlineLabel = findViewById(R.id.ivOnlineIcon);
         ImageView profileLabel = findViewById(R.id.ivProfileIcon);
 
-        chatLabel.setImageResource(R.drawable.ic_chats_selected);
-
-        onlineLabel.setImageResource(R.drawable.ic_online_not_selected);
-        onlineLabel.setOnClickListener(new View.OnClickListener() {
+        chatLabel.setImageResource(R.drawable.ic_chats_not_selected);
+        chatLabel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Intent i = new Intent(HomeActivity.this,OnlineUsersActivity.class);
+                Intent i = new Intent(OnlineUsersActivity.this,HomeActivity.class);
                 i.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(i);
-
             }
         });
+
+        onlineLabel.setImageResource(R.drawable.ic_online_selected);
 
         profileLabel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(HomeActivity.this, ProfileActivity.class);
+                Intent i = new Intent(OnlineUsersActivity.this, ProfileActivity.class);
                 i.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(i);
             }
---
+++ app/src/main/java/com/techknightsrtu/crosstalks/activity/profile/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.techknightsrtu.crosstalks.activity.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.techknightsrtu.crosstalks.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
